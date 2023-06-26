2979d095913577fa333eca2f136357b080c6d2bc
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-06-26 23:07:51 +0200

    .

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 
-const ContactForm = ({ onSubmit }) => {
+const ContactForm = ({ addContact }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    const contact = {
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
+    const newContact = {
       id: nanoid(),
-      name: name,
-      number: number,
+      name: name.trim(),
+      number: number.trim(),
     };
-    onSubmit(contact);
+    addContact(newContact);
     setName('');
     setNumber('');
   };
@@ -25,8 +28,9 @@ const ContactForm = ({ onSubmit }) => {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
+        placeholder="Enter name"
         value={name}
-        onChange={event => setName(event.target.value)}
+        onChange={e => setName(e.target.value)}
       />
       <input
         type="tel"
@@ -34,70 +38,59 @@ const ContactForm = ({ onSubmit }) => {
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
+        placeholder="Enter phone number"
         value={number}
-        onChange={event => setNumber(event.target.value)}
+        onChange={e => setNumber(e.target.value)}
       />
       <button type="submit">Add Contact</button>
     </form>
   );
 };
 
-const ContactList = ({ contacts }) => {
-  return (
-    <ul>
-      {contacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
-    </ul>
-  );
-};
-
-const ContactItem = ({ contact }) => {
-  return (
-    <li>
-      <div>
-        <strong>{contact.name}</strong>
-      </div>
-      <div>{contact.number}</div>
-    </li>
-  );
-};
-
-const Filter = ({ filter, onChange }) => {
-  return (
-    <input
-      type="text"
-      placeholder="Search contacts"
-      value={filter}
-      onChange={onChange}
-    />
-  );
-};
-
-const Phonebook = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-
-  const handleAddContact = contact => {
-    setContacts(prevContacts => [...prevContacts, contact]);
-  };
-
-  const handleFilterChange = event => {
-    setFilter(event.target.value);
-  };
-
+const ContactList = ({ contacts, filter }) => {
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm onSubmit={handleAddContact} />
+    <ul>
+      {filteredContacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name} - {contact.number}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-      <h2>Contacts</h2>
-      <Filter filter={filter} onChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} />
+const Phonebook = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = newContact => {
+    setContacts([...contacts, newContact]);
+  };
+
+  const handleFilterChange = e => {
+    setFilter(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Phone Book</h1>
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={filter}
+        onChange={handleFilterChange}
+      />
+      <ContactForm addContact={addContact} />
+      <ContactList contacts={contacts} filter={filter} />
     </div>
   );
 };
