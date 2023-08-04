afdc8a5da40413c5b6bd2372f15bd2bc5407312f
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-08-04 20:11:46 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import { Provider } from 'react-redux';
-// import { createRoot } from 'react-dom/client';
-// import App from './components/App';
-// import store from './redux/store';
-// import 'react-notifications/lib/notifications.css';
-// import './index.css';
-
-// const rootElement = document.getElementById('root');
-
-// createRoot(rootElement).render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
